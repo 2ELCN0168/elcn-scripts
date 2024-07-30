cf2c236e7b5c75d5f2ba2f222794b20a39ecee37
MH3PZHARXXX001O <157519230+2ELCN0168@users.noreply.github.com>
2024-07-30 10:49:59 +0200

    Modified files

--- 05_System/02_Stack_LAMP/functions/f_install_apache.sh
+++ 05_System/02_Stack_LAMP/functions/f_install_apache.sh
@@ -3,7 +3,7 @@ install_apache() {
         if [[ $OS_TYPE == 'ARCHLINUX' ]]; then
                 echo -e "> ${B_CYAN} Installing Apache2. ${NO_FORMAT}\n"
                 ${PK_INSTALL} --noconfirm apache
-                systemctl enable --now apache
+                systemctl enable --now httpd
         elif [[ $OS_TYPE == 'DEBIAN' ]]; then
                 echo -e "> ${B_RED} Installing Apache2. ${NO_FORMAT}\n"
                 ${PK_INSTALL} -y apache
