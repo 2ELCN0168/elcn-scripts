9dc04851c2c00d770f254663daa4d2b341e8fc29
MH3PZHARXXX001O <157519230+2ELCN0168@users.noreply.github.com>
2024-07-30 10:08:35 +0200

    Modified files

--- 05_System/02_Stack_LAMP/functions/f_install_apache.sh
+++ 05_System/02_Stack_LAMP/functions/f_install_apache.sh
@@ -1,6 +1,9 @@
 install_apache() {
 
-        if [[ $OS_TYPE == 'DEBIAN' || $OS_TYPE == 'ARCHLINUX' ]]; then
+        if [[ $OS_TYPE == 'ARCHLINUX' ]]; then
+                echo -e "> ${B_CYAN} Installing Apache2. ${NO_FORMAT}\n"
+                ${PK_INSTALL} apache
+        elif [[ $OS_TYPE == 'DEBIAN' ]]; then
                 echo -e "> ${B_RED} Installing Apache2. ${NO_FORMAT}\n"
                 ${PK_INSTALL} apache
         elif [[ $OS_TYPE == 'RHEL' ]]; then
